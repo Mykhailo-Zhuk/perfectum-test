6fd6f41f791ce4016dd74a085d26c6fd686d930f
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2024-02-29 22:02:58 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-if (window.innerWidth < 768) {
-  const accordionItems = document.querySelectorAll(".accordion__item");
+const accordionItems = document.querySelectorAll(".accordion__item");
 
-  accordionItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      const content = item.querySelector(".accordion__content");
-      const isOpen = content.style.maxHeight ? true : false;
+accordionItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const content = item.querySelector(".accordion__content");
+    const isOpen = content.style.maxHeight ? true : false;
 
-      accordionItems.forEach((otherItem) => {
-        if (otherItem !== item) {
-          otherItem.classList.remove("is-open");
-          otherItem.querySelector(".accordion__content").style.maxHeight = null;
-        }
-      });
+    accordionItems.forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("is-open");
+        otherItem.querySelector(".accordion__content").style.maxHeight = null;
+      }
+    });
 
+    if (window.innerWidth < 768) {
       item.classList.add("is-open");
       content.style.maxHeight = isOpen ? null : content.scrollHeight + "px";
-    });
+    }
   });
-}
+});
